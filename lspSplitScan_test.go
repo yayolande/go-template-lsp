cea727853770fc36da3b394b5f796c2e2e9d5dcd
yayolande <steve.djumo@protonmail.com>
2024-08-30 11:18:59 +0100

    fix: incomming request parsing is Ok

--- lspSplitScan_test.go
+++ lspSplitScan_test.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"strconv"
 	"strings"
 	"testing"
@@ -11,15 +10,94 @@ import (
 func TestSplitScanLSP(t *testing.T) {
 	tests := []struct {
 		input string
-		want string
+		wants []string
+		isError bool
 	}{
 		{
 			input: "Content-Length: 12\r\n" + "\r\n" + "{'num': 597}",
-			want: "{'num': 597}",
+			wants: []string{"{'num': 597}"},
+		},
+		{
+			input: "Content-Length: 10\r\n" + "\r\n" + "{'num': 597}",
+			wants: []string{"{'num': 59"},
+		},
+		{
+			input: "Content-Type: utf8\r\n" + "\r\n" + "{'num': 597}",
+			wants: []string{""},
+		},
+		{
+			input: "Content-Type: utf8\r\n" + "\r\n" + "{'Content-Length': 22}",
+			wants: []string{""},
+		},
+		{
+			input: "Content-Length: 22\r\n" + "Content-Type: utf8\r\n" + "\r\n" + "{'Content-Length': 27}",
+			wants: []string{"{'Content-Length': 27}"},
+		},
+		{
+			input: "Content-Type: utf8\r\n" + "Content-Length: 22\r\n" + "\r\n" + "{'Content-Length': 27}",
+			wants: []string{"{'Content-Length': 27}"},
+		},
+		{
+			input: "Content-Length 22\r\n" + "Content-Type: utf8\r\n" + "\r\n" + "{'Content-Length': 27}",
+			wants: []string{""},
 		},
 		{
 			input: "Content-Length: 19\r\n" + "Content-Type: utf8\r\n" + "\r\n" + "{'user': 'steveen'}",
-			want: "{'user': 'steveen'}",
+			wants: []string{"{'user': 'steveen'}"},
+		},
+		{
+			input: "Content-Length: 19.2\r\n" + "Content-Type: utf8\r\n" + "\r\n" + "{'user': 'steveen'}",
+			wants: []string{""},
+		},
+		{
+			input: "Content-Length: 19,2\r\n" + "Content-Type: utf8\r\n" + "\r\n" + "{'user': 'steveen'}",
+			wants: []string{""},
+		},
+		{
+			input: "Content-Length: 19\r\n" + "Content-Type: utf8\r\n" + "Authorization: SIMPLE\r\n" + "\r\n" + "{'user': 'steveen'}",
+			wants: []string{"{'user': 'steveen'}"},
+		},
+		{
+			input: "Content-Length: 19\r\n" + "Content-Type: utf8\r\n" + "Authorization: SIMPLE\r\n" + "\r\n" + "{'user': 'steveen'}Garbage data in here",
+			wants: []string{"{'user': 'steveen'}"},
+		},
+		{
+			input: "Content-Length: 0\r\n" + "Content-Type: utf8\r\n" + "Authorization: SIMPLE\r\n" + "\r\n" + "{'user': 'steveen'}Garbage data in here",
+			wants: []string{""},
+		},
+		{
+			input: "Content-Length: -19\r\n" + "Content-Type: utf8\r\n" + "Authorization: SIMPLE\r\n" + "\r\n" + "{'user': 'steveen'}Garbage data in here",
+			wants: []string{""},
+		},
+		{
+			input: "Content-Length: 19\r\n" + "Content-Type: utf8\r\n" + "Authorization: SIMPLE\r\n" + 
+				"\r\n" + "{'user': 'steveen'}" + 
+				"Content-Length: 10\r\n" + "Content-Type: utf8\r\n" + "\r\n" + "{'num': 3}",
+			wants: []string{"{'user': 'steveen'}", "{'num': 3}"},
+		},
+		{
+			input: "Content-Length: 19\r\n" + "Content-Type: utf8\r\n" + "Authorization: SIMPLE\r\n" + 
+				"\r\n" + "{'user': 'steveen'}" + 
+				"Content-Length: 10\r\n" + "Content-Type: utf8\r\n" + "\r\n" + "{'num': 3}",
+			wants: []string{"{'user': 'steveen'}", "{'num': 3}"},
+		},
+		{
+			input: "Content-Length: 19\r\n" + "Content-Type: utf8\r\n" + "Authorization: SIMPLE\r\n" + 
+				"\r\n" + "{'user': 'steveen'}Garbage data in here" + 
+				"Content-Length: 10\r\n" + "Content-Type: utf8\r\n" + "\r\n" + "{'num': 3}",
+			wants: []string{"{'user': 'steveen'}", "{'num': 3}"},
+		},
+		{
+			input: "Content-Type: utf8\r\n" + "Authorization: SIMPLE\r\n" + "Content-Length: 19\r\n" + 
+				"\r\n" + "{'user': 'steveen'}" + 
+				"Content-Type: utf8\r\n" + "Content-Length: 10\r\n" + "\r\n" + "{'num': 3}",
+			wants: []string{"{'user': 'steveen'}", "{'num': 3}"},
+		},
+		{
+			input: "\r\n" + 
+				"\r\n" + "{'user': 'steveen'}" + 
+				"Content-Type: utf8\r\n" + "Content-Length: 10\r\n" + "\r\n" + "{'num': 3}",
+			wants: []string{"", "{'num': 3}"},
 		},
 	}
 
@@ -27,27 +105,28 @@ func TestSplitScanLSP(t *testing.T) {
 		testName := strconv.Itoa(count) + "_" + test.input[:8] + "_test"
 
 		t.Run(testName, func(t *testing.T) {
-			str := test.input
-			scanner := bufio.NewScanner(strings.NewReader(str))
-			scanner.Split(generateScanLspSplitter())
+			scanner := bufio.NewScanner(strings.NewReader(test.input))
+			scanner.Split(inputParsingSplitFunc)
 
-			answer := ""
-			count := 1	// count should never go above 'count + 1'
+			answers := []string{}
 
 			for scanner.Scan() {
-				answer = scanner.Text()
-
-				fmt.Println("Text: ", answer)
-				count ++
+				msg := scanner.Text()
+				answers = append(answers, msg)
 			}
 
-			if scanner.Err() != nil {
-				t.Error("Error while scanning reader: ", scanner.Err())
+			isError := scanner.Err() != nil
+
+			if len(answers) != len(test.wants) {
+				t.Errorf("\n Size of answers (%d) do not match the expected one (%d)", len(answers), len(test.wants))
+				t.Errorf("\n Expected: %v \n Got: %v \n Error: %v", test.wants, answers, scanner.Err())
+				return
 			}
 
-			// TODO: Need to handle the worst case
-			if answer != test.want {
-				t.Errorf("Error -- count : %d\n Expected : %s \n Got : %s", count, test.want, answer)
+			for i := 0; i < len(answers); i++ {
+				if answers[i] != test.wants[i] || isError != test.isError {
+					t.Errorf("\n Expected : %s \n Got : %s \n With error: %v", test.wants[i], answers[i], scanner.Err())
+				} 
 			}
 		})
 	}
